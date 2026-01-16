b105540b0696693e6c5150b611886aa41ebd594f
vahanm-dev <dev.vahanm@gmail.com>
2026-01-16 11:42:45 +0400

    initial commit

---
+++ Chronos/ChronosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronosApp.swift
+//  Chronos
+//
+//  Created by Vahan Muradyan on 16.01.26.
+//
+
+import SwiftUI
+
+@main
+struct ChronosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
