8fabe5f425047ac8451846123a59fb78567b5fac
vahanm-dev <dev.vahanm@gmail.com>
2026-01-16 11:47:36 +0400

    setup project folder structure

---
+++ Chronos/Core/Root/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Chronos
+//
+//  Created by Vahan Muradyan on 16.01.26.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
